3b6234c541cac6ac9b95aefd6f708bf130e1f67b
Felwalker <alex.gordon.gray@gmail.com>
2025-09-22 10:33:06 +1000

    -Removed texture -Make text start large and then collapse inward

--- src/main.c
+++ src/main.c
@@ -17,14 +17,17 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Cold Void");
+	InitWindow(1600, 1280, "Cold Void");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
 	Texture wabbit = LoadTexture("wabbit_alpha.png");
-	
+	int title_x_position = -400;
+	int title_y_position = 320;
+	//Size of the Title
+	int title_size = 600;
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
@@ -33,12 +36,18 @@ int main ()
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
-
-		// draw some text using the default font
-		DrawText("Cold Void", 200,200,20,WHITE);
-
+		//title colapse inward
+		if(title_size > 10){
+			// draw some text using the default font
+		DrawText("Cold Void", title_x_position, title_y_position,title_size,WHITE);
+			// progressively make title size smaller
+			title_x_position++;
+			title_y_position++;
+			title_size--;
+		}
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
+		//DrawTexture(wabbit, 400, 200, WHITE);
+		
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();

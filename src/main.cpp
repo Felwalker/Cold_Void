c1ccd050302a324c33da0ce7d9c2eb7fc63d7484
Felwalker <alex.gordon.gray@gmail.com>
2026-01-02 15:37:57 +1100

    message now appears after a count

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
 
+typedef enum {TITLE, GAMEPLAY} GameScreen;
+
 int main ()
 {
 	// Tell the window to use vsync and work on high DPI displays
@@ -22,13 +24,14 @@ int main ()
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
 	int title_x_position = -400;
 	int title_y_position = 320;
 	//Size of the Title
 	int title_size = 600;
 	char title_text[] = "Cold Void";
+	GameScreen current_screen = TITLE;
+	int frame_count = 0;
+	
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
@@ -37,26 +40,40 @@ int main ()
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
 		ClearBackground(BLACK);
-		//title colapse inward
-		if(title_size > 10){
-			// draw some text using the default font
-		DrawText(title_text, title_x_position, title_y_position,title_size,WHITE);
-			// progressively make title size smaller
-			title_x_position++;
-			title_y_position++;
-			title_size--;
+
+		switch (current_screen){
+			case TITLE:
+			{
+				//title colapse inward
+				if(title_size > 10)	{
+					// draw some text using the default font
+					DrawText(title_text, title_x_position, title_y_position,title_size,WHITE);
+					// progressively make title size smaller
+					title_x_position++;
+					title_y_position++;
+					title_size--;
+				}
+				if (IsKeyPressed(KEY_ENTER))
+				{
+					current_screen = GAMEPLAY;
+					frame_count = 0;
+				}
+				
+			} break;
+			case GAMEPLAY:
+			{
+				DrawText("Thought there would be more to it", 200, 200 , 50, WHITE);
+				if(frame_count > 50) DrawText("Might as well press esc", 200, 260, 50, WHITE);
+			} break;
+			default: break;
 		}
-		// draw our texture to the screen
-		//DrawTexture(wabbit, 400, 200, WHITE);
+		
 		
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
+		frame_count++;
 	}
-
-	// cleanup
-	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();
